b0380d3419e4faa4945fdd0992c2593599d86004
Levi's <110788808+Thiago-Levi@users.noreply.github.com>
2024-03-24 15:16:22 -0300

    Add files via upload

---
+++ Order.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSumaryProject
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; } 
+        public List<OrderItem> OrderItems = new List<OrderItem>();
+
+        public Order(OrderStatus status, Client client)
+        {
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItems.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItems.Remove(item);
+        }
+
+        private double Total()
+        {
+            double total = 0.0;
+            foreach (OrderItem item in OrderItems)
+            {
+                total += item.Subtotal();
+            }
+
+            return total;
+        }
+
+    
+
+        public string OrderSummary()
+        {
+            StringBuilder summary = new StringBuilder();
+
+            summary.AppendLine("Order Summary:");
+            summary.AppendLine($"Order moment:\t{Moment}");
+            summary.AppendLine($"Order status{Status}");
+            summary.AppendLine($"Client: {Client.Name} - ({Client.BirthDate.Year}) - {Client.Email}");
+            summary.AppendLine($"Order Item(s):");
+            foreach (var orderItem in OrderItems)
+            {
+                summary.AppendLine($"Name: {orderItem.Product.Name}, Quantity: {orderItem.Quantity}, Subtotal: {orderItem.Subtotal():C}");
+            }
+            summary.AppendLine($"Total price: {Total():C}");
+        
+            string finalSummary = summary.ToString();
+            return finalSummary;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace OrderSumaryProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+      
+            Console.WriteLine("Enter cliente data ");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            string stringClientBirthDate = Console.ReadLine();
+            DateTime clientBirthDate = DateTime.Parse(stringClientBirthDate);
+            
+
+            Client theClient = new Client(clientName, clientEmail, clientBirthDate);
+            
+            Console.WriteLine("Enter Oder Data:");
+            Console.Write("Status: ");
+            string stringOderStatus = Console.ReadLine();
+            OrderStatus orderStatus = (OrderStatus)Enum.Parse(typeof(OrderStatus), stringOderStatus);
+            Console.Write("Hou many items to this order? ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Order theOrder = new Order(orderStatus, theClient);
+           
+            for (int i = 1; i <= quantity; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                double productPrice = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Quantity: ");
+                int productQuantity = Convert.ToInt32(Console.ReadLine());
+
+                Product theProduct = new Product(productName, productPrice);
+                OrderItem theOrderItem = new OrderItem(productQuantity, productPrice, theProduct);
+                theOrder.AddItem(theOrderItem);
+                
+            }
+
+           string summary = theOrder.OrderSummary();
+           Console.WriteLine(summary);
+            //Console.WriteLine($"{theClient.Name} - {theClient.Email} - {theClient.BirthDate}");
+            Console.ReadLine();
+
+        }
+    }
+}
